125bb2db6cdf54be1eacb70cbdfb7a5c23df1786
Argetim Ramadani <argdev@outlook.com>
2020-01-30 10:03:30 +0100

    Initial commit

---
+++ SimpleDrawing/SimpleDrawing/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleDrawing
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnDraw_Click(object sender, EventArgs e)
+		{
+			Graphics g = this.CreateGraphics();
+			SolidBrush br = new SolidBrush(Color.DarkGray);
+			g.FillEllipse(br, this.ClientRectangle);
+			Pen pn = new Pen(Color.Black, 1);
+			g.DrawEllipse(pn, this.ClientRectangle);
+			pn.Dispose();
+			br.Dispose();
+		}
+	}
+}
